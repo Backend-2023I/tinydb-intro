2bdc01b7e021612d8d6652734e26b79db153a173
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-31 11:34:23 +0500

    crate document object

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from tinydb import TinyDB
+from tinydb.database import Document
 
-db = TinyDB('db.json')
+db = TinyDB('db.json', indent=4)
+
+document = Document({"name": "Javohir", "age": 24}, doc_id=100)
+
+db.insert(document)
